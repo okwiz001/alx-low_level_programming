6ba4573c18f6bb2bc6dd636f14e8a615db3b2cd6
okwiz001 <ogonmalachi01@gmail.com>
2023-10-14 01:37:07 +0100

    my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,5 +5,4 @@ int _isupper(int c)
 	    if (c >= 'A' && c <= 'Z')
 		            return 1;
 	        else
-			        return 0;
-}
+			        return 0;}
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -14,5 +14,4 @@ int main(void)
 		    printf("%c: %d\n", c, _isupper(c));
 		        c = 'a';
 			    printf("%c: %d\n", c, _isupper(c));
-			        return (0);
-}
+			        return (0);}
